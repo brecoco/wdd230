85cc1dbd0cdf26d8e699ceb290dbb80f86d43cea
brecoco <thiagodeoliveiraesilva@gmail.com>
2022-12-12 02:03:45 -0300

    w11

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -1,63 +1,83 @@
-const mbmCompanies = document.querySelector('.mbmCompanies');
+const requestURL = 'https://brecoco.github.io/wdd230/chamber/json_files/companies_list.json';
+const mbmCompany = document.querySelector('.mbmCompanies');
 
-fetch("../chamber/json_files/companies_list.json")
+
+
+fetch(requestURL)
 .then(function(response) {
   return response.json();
 })
 .then(function (jsonObject) {
-  // console.log(jsonObject);
-  // assign variable to JSON array
-  const companies_list = jsonObject['directory'];
-  // Filter for Gold and Silver memberships from spotlightDirectory
-  result = companies_list.filter(obj=> obj.member == "gold" || obj.member == "silver");
+  console.table(jsonObject);  // temporary checking for valid response and data parsing
+  const companies = jsonObject['companies'];
+  result = companies.filter(obj=> obj.member == "gold" || obj.member == "silver");
   // Get 2 random memberships from result
   randSpotLight = getmultRandom(result, 3);
   randSpotLight.forEach(displayspotlights);
 });
-
 function getmultRandom(arr, num) {
   const shuffled = [...arr].sort(() => 0.5 - Math.random());
   return shuffled.slice(0, num);
 }
-  
+
  // create tag and arrange them in order of parentage for index page (spotlight section)
- function displayspotlights(spotlightDirectory) {
-  let randSpotLight = [];
-  let spotlightDiv = document.createElement('div');
-  let h3 = document.createElement('h3');
-  let picture = document.createElement('picture');
-  let imgs = document.createElement('img');
-  let altIMG = document.createElement('alt');
-  let paraQuote = document.createElement('p');
-  let paraAddress = document.createElement('p');
-  let paraPhone = document.createElement('p');
-  let spotA = document.createElement('a');
+ function displayspotlights(company) {
 
-  // h3.textContent = `${spotlightDirectory.businessName}`;
-  spotA.setAttribute('href', spotlightDirectory.websiteURL);
-  spotA.setAttribute('class', 'h3spot');
-  spotA.textContent = `${spotlightDirectory.businessName}`;
-  picture.setAttribute('class', 'spotPicture');
-  imgs.setAttribute('src', spotlightDirectory.logo);
-  imgs.setAttribute('alt', altIMG);
-  imgs.setAttribute('loading', 'lazy');
-  imgs.setAttribute('class', 'spotlightImg');
-  // imgs.setAttribute('width', )
-  altIMG.innerHTML = `${spotlightDirectory.businessName} logo`;
-  paraQuote.textContent = `${spotlightDirectory.quote}`;
-  paraQuote.setAttribute('class', 'spotlightquote');
-  paraAddress.textContent = `${spotlightDirectory.address}`;
-  paraPhone.textContent = `${spotlightDirectory.phone}`;
 
-  h3.appendChild(spotA);
-  spotlightDiv.appendChild(h3);
-  picture.appendChild(imgs);
-  spotlightDiv.appendChild(picture);
-  spotlightDiv.appendChild(paraQuote);
-  spotlightDiv.appendChild(paraAddress);
-  spotlightDiv.appendChild(paraPhone);
+  //div das empresas
+  let mbmCompany = document.createElement('div');
+  mbmCompany.setAttribute('class', 'mbmCompanybox')
+
+
+  //nome das empresas
+  let h2 = document.createElement('h2');
+  h2.setAttribute('class', 'box-titles')
+  let url = document.createElement('a');
+  url.setAttribute('href', company.urlC);
+  url.setAttribute('class', 'website');
+  url.textContent = `${company.name}`;
+  h2.appendChild(url);
+  mbmCompany.appendChild(h2);
+
+
+
+
+  //imagem das empresas
+  let portrait = document.createElement('img'); 
+  portrait.setAttribute('class', 'portrait')
+  portrait.setAttribute('src', company.imageurl);
+  portrait.setAttribute('alt', `Portrait of ${company.name}`);
+  portrait.setAttribute('loading', 'lazy');
+  mbmCompany.appendChild(portrait);
+
+
+
+  //citação das empresas
+  let cDescription = document.createElement('p');
+  cDescription.textContent = `${company.description}`;
+  cDescription.setAttribute('id', 'h4Directory');
+  mbmCompany.appendChild(cDescription);
+
+
+  //endereço das empresas
+  let cAddress = document.createElement('h3');
+  cAddress.textContent = `${company.address}`;
+  mbmCompany.appendChild(cAddress);
+  cAddress.setAttribute('id', 'h3Directory')
+
+
+  //telefone das empresas
+    let cPhone = document.createElement('h3'); 
+  cPhone.textContent = `${company.telephone}`;
+  cPhone.setAttribute('id', 'h3Directory')
+  mbmCompany.appendChild(cPhone);
+
+
+  let lineH = document.createElement('hr');
+  mbmCompany.appendChild(lineH);
   
 
-  document.querySelector('div.nested').appendChild(spotlightDiv);
+  document.querySelector('div.mbmCompanies').appendChild(mbmCompany);
+
 
 }
---
+++ chamber/scripts/spotlight3.js
@@ -0,0 +1,66 @@
+const requestURL = 'https://brecoco.github.io/wdd230/chamber/json_files/companies_list.json';
+const spotlight1 = document.querySelector('.spotlight1');
+
+
+
+fetch(requestURL)
+.then(function(response) {
+  return response.json();
+})
+.then(function (jsonObject) {
+  console.table(jsonObject);  // temporary checking for valid response and data parsing
+  const companies = jsonObject['companies'];
+  result = companies.filter(obj=> obj.member == "gold" || obj.member == "silver");
+  // Get 2 random memberships from result
+  randSpotLight = getmultRandom(result, 1);
+  randSpotLight.forEach(displayspotlights);
+});
+function getmultRandom(arr, num) {
+  const shuffled = [...arr].sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, num);
+}
+
+ // create tag and arrange them in order of parentage for index page (spotlight section)
+ function displayspotlights(company) {
+  let mbmCompany = document.createElement('div');
+  let h2 = document.createElement('h2');
+  h2.setAttribute('class', 'box-titles')
+
+  let picture = document.createElement('picture');
+  let img = document.createElement('img');
+  let altIMG = document.createElement('alt');
+  let paraQuote = document.createElement('p');
+  let paraAddress = document.createElement('p');
+  let paraPhone = document.createElement('p');
+  let spotA = document.createElement('a');
+
+  // h3.textContent = `${spotlightDirectory.businessName}`;
+  h2.setAttribute('class', 'box-titles')
+  spotA.setAttribute('href', company.urlC);
+  spotA.setAttribute('class', 'h3spot');
+  spotA.textContent = `${company.name}`;
+  picture.setAttribute('class', 'spotPicture');
+  img.setAttribute('src', company.imageurl);
+  img.setAttribute('alt', altIMG);
+  img.setAttribute('loading', 'lazy');
+  img.setAttribute('id', 'congressimg');
+  // imgs.setAttribute('width', )
+  altIMG.innerHTML = `${company.name} logo`;
+  paraQuote.textContent = `${company.description}`;
+  paraQuote.setAttribute('class', 'spotlightquote');
+  paraAddress.textContent = `${company.address}`;
+  paraPhone.textContent = `${company.telephone}`;
+
+  h2.appendChild(spotA);
+  mbmCompany.appendChild(h2);
+  picture.appendChild(img);
+  mbmCompany.appendChild(picture);
+  mbmCompany.appendChild(paraQuote);
+  mbmCompany.appendChild(paraAddress);
+  mbmCompany.appendChild(paraPhone);
+  
+
+  document.querySelector('div.spotlight1').appendChild(mbmCompany);
+
+
+}
